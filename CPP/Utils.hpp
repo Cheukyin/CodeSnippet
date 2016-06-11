82e30f256cfec3040f5efe71839ddd0d8ca98639
Cheukyin Yip <cheukyinyip0077@gmail.com>
2016-06-11 12:46:21 +0800

    EnableIf

--- CPP/Utils.hpp
+++ CPP/Utils.hpp
@@ -2,16 +2,44 @@
 
 namespace CYTL
 {
+    // Type Displayer
+    // usage: TypeDisplayer<decltype(t)> tType;
+    //        then see the compiler's error info
+    template<class T> class TypeDisplayer;
+
+
+    // check if T1 == T2
+    template<class T1, class T2> struct StaticTypeCheckEQ;
+    template<class T> struct StaticTypeCheckEQ<T, T> {};
+
 
     // return size of an array as a constant-time constant
-    template<typename T, std::size_t N>
+    template<class T, std::size_t N>
     constexpr std::size_t arraySize(T (&)[N]) noexcept
     { return N; }
 
 
-    // Type Displayer
-    // usage: TypeDisplayer<decltype(t)> tType;
-    //        then see the compling error info
-    template<typename T> class TypeDisplayer;
+    // if B, T1, else T2
+    template<bool B, class T1, class T2> struct _Select;
+    template<class T1, class T2>
+    struct _Select<true, T1, T2>
+    { typedef T1 type; };
+    template<class T1, class T2>
+    struct _Select<false, T1, T2>
+    { typedef T2 type; };
+
+    template<bool B, class T1, class T2>
+    using IfThenElse = typename _Select<B, T1, T2>::type;
+
+
+    // ------------------------------------
+    // EnableIf
+    template<bool B, class T> struct _EnableIf;
+    template<class T>
+    struct _EnableIf<true, T>
+    { typedef T type; };
+
+    template<bool B, class T>
+    using EnableIf = typename _EnableIf<B, T>::type;
 
 } // namespace CYTL
--- CPP/UtilsTest.cpp
+++ CPP/UtilsTest.cpp
@@ -1,15 +1,29 @@
 #include "Utils.hpp"
 #include "UnitTest.hpp"
 
+// TEST_CASE(TypeDisplayer)
+// {
+//     auto x = {3};
+//     CYTL::TypeDisplayer<decltype(x)> xType;
+// }
+
+
 TEST_CASE(arraySize)
 {
     int keyVals[] = {1, 2, 3, 4, 5, 6};
     static_assert(CYTL::arraySize(keyVals) == 6, "arraySize error");
 }
 
+TEST_CASE(IfThenElse)
+{
+    struct T; struct F;
 
-// TEST_CASE(TypeDisplayer)
-// {
-//     auto x = {3};
-//     CYTL::TypeDisplayer<decltype(x)> xType;
-// }
+    CYTL::StaticTypeCheckEQ<CYTL::IfThenElse<true, T, F>, T>();
+    CYTL::StaticTypeCheckEQ<CYTL::IfThenElse<false, T, F>, F>();
+}
+
+TEST_CASE(EnableIf)
+{
+    struct T;
+    CYTL::StaticTypeCheckEQ<CYTL::EnableIf<true, T>, T>();
+}
