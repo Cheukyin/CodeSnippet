5f53797c19b9fe99ddf0d626dabbae86703c5ac5
Cheukyin Yip <cheukyinyip0077@gmail.com>
2016-06-16 21:54:36 +0800

    Cyclic Visitor

--- CPP/Visitor.hpp
+++ CPP/Visitor.hpp
@@ -8,38 +8,63 @@ namespace CYTL
     // ---------------------------------------
     // Acyclic Visitor
 
+    // AcyclicBaseVisitor
     struct AcyclicBaseVisitor
     { virtual ~AcyclicBaseVisitor() {} };
 
+    // AcyclicVisitor
     template<class VisitableType, class ReturnType = void>
     struct AcyclicVisitor
-    { virtual ReturnType Visit(VisitableType&) = 0; };
+    { virtual ReturnType visit(VisitableType&) = 0; };
 
+    // BaseVisitable
     template<class ReturnType = void>
     struct BaseVisitable
     {
         virtual ~BaseVisitable() {}
-        virtual ReturnType Accept(AcyclicBaseVisitor&) = 0;
+        virtual ReturnType accept(AcyclicBaseVisitor&) = 0;
 
     protected:
         template<class VisitableType>
-        static ReturnType AcceptImpl(VisitableType& visited, AcyclicBaseVisitor& guest)
+        static ReturnType acceptImpl(VisitableType& visited, AcyclicBaseVisitor& visitor)
         {
             using VisitorType = AcyclicVisitor<VisitableType, ReturnType>;
-            if(VisitorType* p = dynamic_cast<VisitorType*>(&guest))
-                return p->Visit(visited);
+            if(VisitorType* p = dynamic_cast<VisitorType*>(&visitor))
+                return p->visit(visited);
             return ReturnType();
         }
     };
 
     #define DefineAcyclicAccept() \
-        virtual auto Accept(CYTL::AcyclicBaseVisitor& guest) \
-            -> decltype(AcceptImpl(*this, guest)) \
-        { return AcceptImpl(*this, guest); }
+        virtual auto accept(CYTL::AcyclicBaseVisitor& visitor) \
+            -> decltype(acceptImpl(*this, visitor)) \
+        { return acceptImpl(*this, visitor); }
 
 
     // ---------------------------------------
     // Cyclic Visitor
+
+    // CyclicVisitorUnit
+    template<class VisitableType, class ReturnType = void>
+    struct CyclicVisitorUnit
+    {
+        virtual ReturnType visit(VisitableType&) = 0;
+    	virtual ~CyclicVisitorUnit() {}
+    };
+
+    // CyclicVisitor
+    template<class L, class ReturnType = void>
+    struct CyclicVisitor: GenScatterHierarchy<L, TypeBind2nd<CyclicVisitorUnit, ReturnType>::template Type>
+    {
+        template<class VisitableType>
+        ReturnType visit(VisitableType& visitable)
+        { return static_cast<CyclicVisitorUnit<VisitableType, ReturnType>&>(*this).visit(visitable); }
+    };
+
+    #define DefineCyclicAccept(CyclicVisitorType) \
+        virtual auto accept(CyclicVisitorType& visitor) \
+            -> decltype( visitor.visit(*this) ) \
+        { return visitor.visit(*this); }
 
 } // namespace CYTL
 
--- CPP/VisitorTest.cpp
+++ CPP/VisitorTest.cpp
@@ -15,8 +15,8 @@ namespace AcyclicVisitorTest
         public CYTL::AcyclicVisitor<N1, int>,
         public CYTL::AcyclicVisitor<N2, std::string>
     {
-        int Visit(N1& n) { return n.v; }
-        std::string Visit(N2& n) { return n.v; }
+        int visit(N1& n) { return n.v; }
+        std::string visit(N2& n) { return n.v; }
     };
 
     struct V2:
@@ -24,8 +24,8 @@ namespace AcyclicVisitorTest
         public CYTL::AcyclicVisitor<N1, int>,
         public CYTL::AcyclicVisitor<N2, std::string>
     {
-        int Visit(N1& n) { return n.v + 1; }
-        std::string Visit(N2& n) { return n.v + "2"; }
+        int visit(N1& n) { return n.v + 1; }
+        std::string visit(N2& n) { return n.v + "2"; }
     };
 }
 TEST_CASE(AcyclicVisitor)
@@ -34,10 +34,56 @@ TEST_CASE(AcyclicVisitor)
     AcyclicVisitorTest::N2 n2;
 
     AcyclicVisitorTest::V1 v1;
-    EXPECT_EQ((&n1)->Accept(v1), 1);
-    EXPECT_EQ((&n2)->Accept(v1), "N2");
+    EXPECT_EQ((&n1)->accept(v1), 1);
+    EXPECT_EQ((&n2)->accept(v1), "N2");
 
     AcyclicVisitorTest::V2 v2;
-    EXPECT_EQ((&n1)->Accept(v2), 2);
-    EXPECT_EQ((&n2)->Accept(v2), "N22");
+    EXPECT_EQ((&n1)->accept(v2), 2);
+    EXPECT_EQ((&n2)->accept(v2), "N22");
+}
+
+
+namespace CyclicVisitorTest
+{
+    struct N1; struct N2; struct N3;
+    using TestVisitor = CYTL::CyclicVisitor<CYTL::TypeList<N1, N2, N3>, std::string>;
+
+    struct N1
+    { std::string v{"N1"}; DefineCyclicAccept(TestVisitor) };
+
+    struct N2
+    { std::string v{"N2"}; DefineCyclicAccept(TestVisitor) };
+
+    struct N3
+    { std::string v{"N3"}; DefineCyclicAccept(TestVisitor) };
+
+    struct V1: public TestVisitor
+    {
+        std::string visit(N1& n) { return n.v; }
+        std::string visit(N2& n) { return n.v; }
+        std::string visit(N3& n) { return n.v; }
+    };
+
+    struct V2: public TestVisitor
+    {
+        std::string visit(N1& n) { return n.v + "2"; }
+        std::string visit(N2& n) { return n.v + "2"; }
+        std::string visit(N3& n) { return n.v + "2"; }
+    };
+}
+TEST_CASE(CyclicVisitor)
+{
+    CyclicVisitorTest::N1 n1;
+    CyclicVisitorTest::N2 n2;
+    CyclicVisitorTest::N3 n3;
+
+    CyclicVisitorTest::V1 v1;
+    EXPECT_EQ((&n1)->accept(v1), "N1");
+    EXPECT_EQ((&n2)->accept(v1), "N2");
+    EXPECT_EQ((&n3)->accept(v1), "N3");
+
+    CyclicVisitorTest::V2 v2;
+    EXPECT_EQ((&n1)->accept(v2), "N12");
+    EXPECT_EQ((&n2)->accept(v2), "N22");
+    EXPECT_EQ((&n3)->accept(v2), "N32");
 }
