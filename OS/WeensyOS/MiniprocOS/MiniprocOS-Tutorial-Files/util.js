9a78391fc4a37316ae9e93c7a1efaab824e7609d
Cheukyin Yip <cheukyinyip0077@gmail.com>
2016-06-29 21:35:28 +0800

    MiniProcsOS Tutorial

---
+++ OS/WeensyOS/MiniprocOS/MiniprocOS-Tutorial-Files/util.js
@@ -0,0 +1,49 @@
+<?xml version="1.0" encoding="ISO-8859-1"?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
+  "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" lang="en" xml:lang="en">
+<head>
+<title>Object not found!</title>
+<link rev="made" href="mailto:helpdesk@cims.nyu.edu" />
+<style type="text/css"><!--/*--><![CDATA[/*><!--*/ 
+    body { color: #000000; background-color: #FFFFFF; }
+    a:link { color: #0000CC; }
+    p, address {margin-left: 3em;}
+    span {font-size: smaller;}
+/*]]>*/--></style>
+</head>
+
+<body>
+<h1>Object not found!</h1>
+<p>
+
+
+    The requested URL was not found on this server.
+
+  
+
+    The link on the
+    <a href="https://www.cs.nyu.edu/~mwalfish/classes/15sp/labs/lab5.html">referring
+    page</a> seems to be wrong or outdated. Please inform the author of
+    <a href="https://www.cs.nyu.edu/~mwalfish/classes/15sp/labs/lab5.html">that page</a>
+    about the error.
+
+  
+
+</p>
+<p>
+If you think this is a server error, please contact
+the <a href="mailto:helpdesk@cims.nyu.edu">webmaster</a>.
+
+</p>
+
+<h2>Error 404</h2>
+<address>
+  <a href="/">www.cs.nyu.edu</a><br />
+  
+  <span>Wed Jun 29 09:31:23 2016<br />
+  Apache/2.2.15 (Red Hat)</span>
+</address>
+</body>
+</html>
+
